00b1d0b7d1e991a41d11142c5ca228e2c16ab5e2
Raselj71 <raselj44@gmail.com>
2025-07-23 14:18:53 +0600

    added auth module

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { userDto } from './dto/userDto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+     constructor(private authService:AuthService){}
+
+    @Post('sign-in')
+    signIn(@Body() userDto:userDto){
+        return this.authService.userSignIn(userDto)
+
+
+
+    }
+
+}
--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { userDto } from './dto/userDto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
+
+    async userSignIn(userDto: userDto) {
+
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: userDto.email
+            }
+        })
+
+
+    }
+
+
+}
